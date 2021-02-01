3ae6c470920214cd88a422912b659f167397e692
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-01 00:37:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,9 +4,6 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array_names)
-  badge_maker = badges
-  array_names.each do |name|
-    array_messages << badges
-  end
-  return array_messages
+  
+  
 end
